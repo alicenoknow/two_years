1064f22a58b10e6945965580746bfd02fef982c8
Alicja Niewiadomska <micek2000@gmail.com>
2023-10-19 14:26:40 +0100

    idk nie dziala

--- components/ImageWithLoading.tsx
+++ components/ImageWithLoading.tsx
@@ -4,20 +4,16 @@ import Image from "next/image";
 
 const IMAGE_SIZE = 300;
 
-
 const ImageWithLoading = ({ imgUrl }: { imgUrl: string }) => {
     return (<Image
         src={imgUrl}
         alt="Image or GIF"
         width={IMAGE_SIZE}
         height={IMAGE_SIZE}
-        placeholder="blur"
-        blurDataURL="/images/blur.webp"
         priority
         loading="eager"
         />
     );
 }
-
 
 export default ImageWithLoading;
