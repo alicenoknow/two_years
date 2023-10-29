f9694e317b89a7684550ea7907e569167e0944c9
Alicja Niewiadomska <micek2000@gmail.com>
2023-10-29 22:34:42 +0000

    Copy changes

--- components/Emojis.tsx
+++ components/Emojis.tsx
@@ -1,5 +1,5 @@
 
 import React from "react";
 
-export const HeartEmoji = () => <div className="px-6"><div className="heart text-3xl z-0 opacity-70">💗</div></div>
-export const CroissantEmoji = () => <div className="px-6"><div className="croissant text-3xl z-0 opacity-70">🥐</div></div>
+export const HeartEmoji = () => <div className="px-12"><div className="heart text-5xl z-0 opacity-70">💗</div></div>
+export const CroissantEmoji = () => <div className="px-12"><div className="croissant text-5xl z-0 opacity-70">🥐</div></div>
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -4,7 +4,10 @@ const Welcome = () => {
   return (
     <div className="flex items-center justify-center z-10 px-96 pt-24">
       <CroissantEmoji />
-      <h1 className="mb-4 text-4xl font-extrabold z-10 leading-[4rem] text-gray-900 dark:text-white md:text-4xl lg:text-4xl">Dokładnie 2 lata temu podjąłeś <span className="text-transparent z-10 bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 text-4xl">najlepszą decyzję w swoim życiu.</span> Teraz nadszedł moment, żeby ponownie zweryfikować czy ta relacja dalej ma sens.</h1>
+      <h1 className="mb-4 text-4xl font-extrabold z-10 leading-[4rem] text-gray-900 dark:text-white md:text-4xl lg:text-4xl">Dokładnie 2 lata temu podjąłeś 
+        <span className="text-transparent z-10 bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 text-4xl"> najlepszą decyzję w swoim życiu. </span>
+        Teraz nadszedł moment, aby zweryfikować czy ta relacja przetrwała próbę czasu i dalej ma sens.
+      </h1>
     </div>
   );
 };
